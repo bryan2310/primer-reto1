fbe3e7e1be9adf3aae17be04052e4708aa99fc6a
bryan2310 <bryanavila_2310@outlook.com>
2024-09-13 19:30:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
-meme_dict = {
-            "CRINGE": "Algo excepcionalmente raro o embarazoso",
-            "LOL": "Una respuesta común a algo gracioso",
-            }
-word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("esa palabra no esta en el dicionario")
+import discord
+import random
+from discord.ext import commands
+from bot_logic import gen_pass
+from bot_emodji import gen_emodji
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"soy un nuevo bot porfavor usa un ! antes de decir algo {bot.user}")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def emodji(ctx):
+    await ctx.send(gen_emodji)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+bot.run("MTI4MTc3OTgxMDA1NzU4ODc2Nw.GhFjB9.AX3Y8llDAQ8EjOJZmCiekYPNS0zMTE76u1YBqs")
