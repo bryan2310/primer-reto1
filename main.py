41a48dac6ba91e75a2a93e0b42f8d0a05c26d7c7
bryan2310 <bryanavila_2310@outlook.com>
2024-10-04 14:37:54 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import discord
-import random
 from discord.ext import commands
-from bot_logic import gen_pass
-from bot_emodji import gen_emodji
 import os
-# La variable intents almacena los privilegios del bot
+
 intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
@@ -17,48 +14,28 @@ async def on_ready():
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send(f'Hola, soy un bot {bot.user}!')
+    await ctx.send(f'Hola, soy un bot pon materiales reutilisables y te ejecutare una maqueta{bot.user}!')
 
 @bot.command()
-async def password(ctx):
-    await ctx.send(gen_pass(10))
-
-@bot.command()
-async def emodji(ctx):
-    await ctx.send(gen_emodji)
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+async def carton(ctx):
     
-@bot.command()
-async def repeat(ctx, times: int, content='repeating...'):
-    for i in range(times):
-        await ctx.send(content)
-@bot.command()
-async def mem(ctx):
-    with open('images/mem1.jpg', 'rb') as f:
-        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
-        picture = discord.File(f)
+    await ctx.send(image_url0,"recolecta carton de tu casa y ayuda a tu hijo a crear casas")
+    picture = discord.File(f)
     # A continuación, podemos enviar este archivo como parámetro.
     await ctx.send(file=picture)
-
 @bot.command()
-async def mem_aleatorio(ctx):
-    meme_aleatorio=random.choice(os.listdir("images"))
-    with open(f'images/{meme_aleatorio}', 'rb') as f:
-        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
-        picture = discord.File(f)
+async def plastico(ctx):
+    
+    await ctx.send(image_url1,"recicla muchas tapas de plastico y crea un jugete para tu hijo y a la vez ayudas al planeta")
+    picture = discord.File(f)
     # A continuación, podemos enviar este archivo como parámetro.
     await ctx.send(file=picture)
-
-
+@bot.command()
+async def botellas(ctx):
+    image_url3 = get_maqueta3_image_url
+    await ctx.send(image_url3, "atentos a lus adultos amantes de las plantas al cortar la botella a la mitad")
+    picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
 bot.run("")
+
